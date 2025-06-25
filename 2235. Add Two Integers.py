f9e2322948a8d20cb35b63a4474e135a44facf86
shawkath73 <shawkathvimuhammed@gmail.com>
2025-06-25 16:07:15 +0530

    Easy

---
+++ 2235. Add Two Integers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        sum=num1+num2
+        return sum
