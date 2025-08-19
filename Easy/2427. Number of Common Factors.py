9732df52a2b6ae6938505545208f4a34442ce057
Shawkath_Muhammed <shawkathvimuhammed@gmail.com>
2025-08-19 19:34:40 +0530

    Update 2427. Number of Common Factors.py

--- Easy/2427. Number of Common Factors.py
+++ Easy/2427. Number of Common Factors.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
+        
         factors=0
         for i in range(1,min(a,b)+1):
             if a % i == 0 and b % i == 0:
